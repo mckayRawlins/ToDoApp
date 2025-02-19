71111315070f2ae5d263e4266de321e4df2a93cd
Miriam Rawlins <miriammrawlins@gmail.com>
2025-02-18 19:53:14 -0700

    practicing classes

--- README.md
+++ README.md
@@ -22,3 +22,86 @@
             </div>
         
     </main>
+
+    /* const groups = [];
+
+class TaskItem {
+    constructor(label, group) {
+        this.id = Date.now();
+        this.label = label;
+        this.group = group;
+    }
+
+    editTask() {
+        console.log('edit');
+    }
+    completeTask() {
+        console.log('completed');
+    }
+}
+
+function renderGroups() {
+
+}
+
+function addGroup() {
+    let addGroupTitle = document.getElementById('create-group-input');
+    addGroupTitle.value = addGroupTitle.value.trim();
+    if (addGroupTitle.value === "") {
+        return;
+    } 
+
+    const newGroup = stickyNoteTemplate.content.cloneNode(true).children[0];
+    const groupsContainer = document.getElementById('groups-container');
+    const newTaskList = document.createElement('ul');
+    newTaskList.id = addGroupTitle.value;
+
+    let groupName = newGroup.querySelector('.group-name');
+
+    groupName.innerText = addGroupTitle.value;
+    newGroup.appendChild(newTaskList);
+    groupsContainer.appendChild(newGroup);
+
+    addGroupTitle.value = '';
+
+}
+
+
+
+function addTask(event) {
+
+    const groupDiv = event.target.parentElement; // Find which "Add" button was clicked
+    let newTaskInput = groupDiv.querySelector('#new-task-input');
+    newTaskInput.value = newTaskInput.value.trim();
+    const taskList = groupDiv.querySelector('ul'); // Find the task list inside this group
+    const task = new TaskItem(newTaskInput.value, taskList.id)
+    console.log(task);
+
+    if (newTaskInput.value === "") {
+        alert('Please enter task')
+        return;
+    }
+
+    const listItem = document.createElement('li');
+    const editTask = document.createElement('button');
+    editTask.addEventListener('click', () => {
+        console.log('edit');
+    })
+    const deleteTask = document.createElement('button');
+    deleteTask.addEventListener('click', () => {
+        console.log('delete');
+    })
+    listItem.innerText = newTaskInput.value;
+    deleteTask.innerText = "Delete";
+    editTask.innerText = "Edit"
+    taskList.appendChild(listItem);
+    listItem.appendChild(editTask);
+    listItem.appendChild(deleteTask);
+
+
+    groups.push(task);
+    console.log(groups);
+
+    newTaskInput.value = '';
+}
+ */
--- Scripts/index.js
+++ Scripts/index.js
@@ -1,88 +1,41 @@
-
-
-
-
-
-
-/* const groups = [];
-
 class TaskItem {
-    constructor(label, group) {
+    constructor(taskName) {
+        this.taskName = taskName;
         this.id = Date.now();
-        this.label = label;
-        this.group = group;
-    }
-
-    editTask() {
-        console.log('edit');
-    }
-    completeTask() {
-        console.log('completed');
     }
 }
 
-function renderGroups() {
-
-}
-
-function addGroup() {
-    let addGroupTitle = document.getElementById('create-group-input');
-    addGroupTitle.value = addGroupTitle.value.trim();
-    if (addGroupTitle.value === "") {
-        return;
-    } 
-
-    const newGroup = stickyNoteTemplate.content.cloneNode(true).children[0];
-    const groupsContainer = document.getElementById('groups-container');
-    const newTaskList = document.createElement('ul');
-    newTaskList.id = addGroupTitle.value;
-
-    let groupName = newGroup.querySelector('.group-name');
-
-    groupName.innerText = addGroupTitle.value;
-    newGroup.appendChild(newTaskList);
-    groupsContainer.appendChild(newGroup);
-
-    addGroupTitle.value = '';
-
-}
-
-
-
-function addTask(event) {
-
-    const groupDiv = event.target.parentElement; // Find which "Add" button was clicked
-    let newTaskInput = groupDiv.querySelector('#new-task-input');
-    newTaskInput.value = newTaskInput.value.trim();
-    const taskList = groupDiv.querySelector('ul'); // Find the task list inside this group
-    const task = new TaskItem(newTaskInput.value, taskList.id)
-    console.log(task);
-
-    if (newTaskInput.value === "") {
-        alert('Please enter task')
-        return;
+class TaskGroup {
+    constructor(groupName) {
+        this.groupName = groupName;
     }
 
-    const listItem = document.createElement('li');
-    const editTask = document.createElement('button');
-    editTask.addEventListener('click', () => {
-        console.log('edit');
-    })
-    const deleteTask = document.createElement('button');
-    deleteTask.addEventListener('click', () => {
-        console.log('delete');
-    })
-    listItem.innerText = newTaskInput.value;
-    deleteTask.innerText = "Delete";
-    editTask.innerText = "Edit"
-    taskList.appendChild(listItem);
-    listItem.appendChild(editTask);
-    listItem.appendChild(deleteTask);
-
-
-    groups.push(task);
-    console.log(groups);
-
-    newTaskInput.value = '';
+    taskItems = [];
 }
- */
+
+
+const houseChores = new TaskGroup('house chores');
+const cleanDishes = new TaskItem('clean the dishes');
+const batheDog = new TaskItem('bathe the dog');
+houseChores.taskItems.push(cleanDishes);
+houseChores.taskItems.push(batheDog);
+
+const lawnAndGarden = new TaskGroup('lawn and garden');
+const mowLawn = new TaskItem('mow the lawn');
+const trimBushes = new TaskItem('trim the bushes');
+lawnAndGarden.taskItems.push(mowLawn);
+lawnAndGarden.taskItems.push(trimBushes);
+
+const birthdayTasks = new TaskGroup('birthday party');
+const cake = new TaskItem('make the cake');
+const decore = new TaskItem('decorate house');
+birthdayTasks.taskItems.push(cake);
+birthdayTasks.taskItems.push(decore);
+
+lawnAndGarden.taskItems.forEach(forEachTask);
+houseChores.taskItems.forEach(forEachTask);
+birthdayTasks.taskItems.forEach(forEachTask);
+
+function forEachTask(taskItem) {
+    console.log(taskItem.taskName);
+}
