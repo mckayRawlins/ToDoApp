fa389d4b722c526420621e3219e7268db23ff557
Jason Rawlins <jasonrawlins@hotmail.com>
2025-02-22 21:37:53 -0700

    Core code added for most functionality.

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,7 +1,161 @@
+class TodoApp {
+    taskGroupsDB = [];
+
+    renderUI() {
+        const taskGroupUl = this.taskGroupUl();
+        taskGroupUl.innerHTML = '';
+        this.taskItemsUl().innerHTML = '';
+    
+        this.taskGroupsDB.forEach(taskGroup => {
+            const taskGroupLi = document.createElement('li');
+            taskGroupLi.taskGroup = taskGroup;
+            taskGroupLi.innerText = taskGroup.name;
+            
+            if (taskGroup.selected) {
+                taskGroupLi.classList.add('selected');
+                var taskGroupNameElement = this.getElement('task-group-name');
+                taskGroupNameElement.innerText = taskGroup.name;
+                taskGroup.taskItems.forEach(this.renderTaskItems.bind(this));
+            }
+ 
+            taskGroupUl.appendChild(taskGroupLi);
+            taskGroupLi.addEventListener('click', this.taskGroupLiClicked.bind(this));
+        });
+        
+        this.save();
+    }
+
+    renderTaskItems(taskItem) {
+        const taskItemLi = document.createElement('li');
+        taskItemLi.taskItem = taskItem;
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+
+        if (taskItem.completed) {
+            checkbox.checked = true; 
+        }
+
+        checkbox.addEventListener('click', () => {
+            taskItem.completed = !taskItem.completed;
+        });
+
+        const nameSpan = document.createElement('span');
+        nameSpan.innerHTML = taskItem.name;
+
+        taskItemLi.appendChild(checkbox);
+        taskItemLi.appendChild(nameSpan);
+
+        this.taskItemsUl().appendChild(taskItemLi);
+    }
+
+    clearCompletedTasks() {
+        const selectedTaskGroup = this.selectedTaskGroup();
+        selectedTaskGroup.taskItems = selectedTaskGroup.taskItems.filter(listItem => !listItem.completed);
+
+        this.renderUI();
+    }
+
+    removeTaskGroup() {
+        const selectedTaskGroup = this.selectedTaskGroup();
+        this.taskGroupsDB = this.taskGroupsDB.filter(taskGroup => taskGroup.name !== selectedTaskGroup.name);
+
+        if (this.taskGroupsDB.length > 0) {
+            this.taskGroupsDB.forEach(taskGroup => taskGroup.selected = false);
+            this.taskGroupsDB[0].selected = true;
+            this.save();
+        } else {
+            this.load();
+        }
+
+        this.renderUI();
+    }
+
+    taskGroupLiClicked(mouseEvent) {
+        for (let taskGroupLi of this.taskGroupUl().children) {
+            taskGroupLi.taskGroup.selected = false;
+            taskGroupLi.classList.remove('selected');
+        }
+
+        const taskGroupLi = mouseEvent.target;
+        taskGroupLi.taskGroup.selected = true;       
+        taskGroupLi.classList.add('selected');
+            
+        this.renderUI()
+    }
+
+    addGroup() {
+        const newTaskGroupInput = this.getElement('new-group-input');
+
+        if (newTaskGroupInput.value.length > 0) {
+            this.taskGroupsDB.forEach(taskGroup => taskGroup.selected = false)
+
+            const newTaskGroup = new TaskGroup(newTaskGroupInput.value);
+            newTaskGroupInput.value = '';
+            newTaskGroup.selected = true;
+            this.taskGroupsDB.push(newTaskGroup);
+            this.renderUI();
+        }
+    }
+
+    addTask() {
+        const newTaskInput = this.getElement('new-task-input');
+
+        if (newTaskInput.value.length > 0) {
+            this.selectedTaskGroup().taskItems.push(new TaskItem(newTaskInput.value));
+            newTaskInput.value = '';
+            this.renderUI();
+        }
+    }
+
+    getElement(id) {
+        return document.getElementById(id);
+    } 
+
+    taskGroupUl() {
+        return this.getElement('task-group-ul');
+    }
+
+    taskItemsUl() {
+        return this.getElement('task-items');
+    }
+
+    selectedTaskGroup() {
+        return this.taskGroupsDB.find(taskGroup => taskGroup.selected);
+    }
+
+    save() {
+        localStorage.setItem('task-groups', JSON.stringify(this.taskGroupsDB));
+    }
+
+    load() {
+        const savedTaskGroups = localStorage.getItem('task-groups');
+
+        if (savedTaskGroups === null) {
+            localStorage.setItem('task-groups', JSON.stringify([defaultTaskGroup()]));
+        }
+
+        this.taskGroupsDB = JSON.parse(localStorage.getItem('task-groups'));
+    }
+
+    start() {
+        this.load();
+        this.renderUI();
+    }
+
+    defaultTaskGroup() {
+        const defaultTodo = new TaskGroup("Todo");
+        defaultTodo.taskItems.push(new TaskItem("Task 1"));
+        defaultTodo.taskItems.push(new TaskItem("Task 2"));
+        return defaultTodo;
+    }
+}
+
 class TaskItem {
     constructor(name) {
         this.name = name;
         this.id = Date.now();
+        this.completed = false;
     }
 }
 
@@ -11,122 +165,8 @@ class TaskGroup {
     }
 
     taskItems = [];
-    selected = false;
+    selected = true;
 }
 
-let taskGroupsDB = [];
-refreshUI();
-
-function getElement(id) {
-    return document.getElementById(id);
-}
-
-function refreshUI() {
-    var taskGroupUl = getElement('task-group-ul');
-    taskGroupUl.innerHTML = '';
-
-    taskGroupsDB.forEach(taskGroup => {
-        const taskGroupLi = document.createElement('li');
-        taskGroupLi.taskGroup = taskGroup;
-        taskGroupLi.innerText = taskGroup.name;
-        taskGroupLi.addEventListener('click', () => {
-            taskGroupLi.classList.add('selected');
-            selectTaskGroup(taskGroup.name);
-        });
-
-        taskGroupUl.appendChild(taskGroupLi);
-        taskGroup.taskItems.forEach(taskItemForeach);
-    });
-}
-
-function taskItemForeach(taskItem) {
-    const taskItemLi = document.createElement('li');
-    taskItemLi.taskItem = taskItem;
-    taskItemLi.innerText = taskItem.name;
-    taskItemLi.addEventListener('click', () => {
-        console.log(`Task Item ${taskItem.name} is complete.`);
-    });
-
-    tasksUl.appendChild(taskItemLi);
-}
-
-// function createLi(propertyName, liObject, clickHandler) {
-//     const li = document.createElement('li');
-//     li[propertyName] = liObject;
-//     li.innerText = liObject.name;
-//     li.addEventListener('click', clickHandler);
-
-//     tasksUl.appendChild(li);
-// }
-
-// function selectNewestGroup() {
-//     const taskGroupUl = getElement('task-group-ul');
-
-//     var newestGroup = taskGroupUl.children[taskGroupUl.children.length - 1];
-//     newestGroup.taskGroup.selected = true;
-//     newestGroup.classList.add('selected');
-// }
-
-function selectTaskGroup(taskGroupName) {
-    const taskGroupUl = getElement('task-group-ul');
-
-    for (let li of taskGroupUl.children) {
-        li.taskGroup.selected = false;
-        li.classList.remove('selected');
-    }
-
-    for (let li of taskGroupUl.children) {
-        if (li.taskGroup.name === taskGroupName) {
-            li.taskgGroup.selected = true;
-            li.classList.add('selected');
-        }
-    }
-}
-
-function addGroup() {
-    const newGroupInput = getElement('new-group-input');
-    // clear all selected from the current list
-
-    taskGroupsDB.push(new TaskGroup(newGroupInput.value, true));
-    newGroupInput.value = '';
-    refreshUI();
-    selectNewestGroup();
-}
-
-function addNewTask() {
-    const newTaskInput = getElement('new-task-input');
-    // Add a new TaskItem to the first taskgroup in the database with the user's input
-    taskGroupsDB[0].taskItems.push(new TaskItem(newTaskInput.value));
-    newTaskInput.value = '';
-    refreshUI();
-}
-
-
-/* 
-const houseChores = new TaskGroup('house chores');
-const cleanDishes = new TaskItem('clean the dishes');
-const batheDog = new TaskItem('bathe the dog');
-houseChores.taskItems.push(cleanDishes);
-houseChores.taskItems.push(batheDog);
-
-const lawnAndGarden = new TaskGroup('lawn and garden');
-const mowLawn = new TaskItem('mow the lawn');
-const trimBushes = new TaskItem('trim the bushes');
-lawnAndGarden.taskItems.push(mowLawn);
-lawnAndGarden.taskItems.push(trimBushes);
-
-const birthdayTasks = new TaskGroup('birthday party');
-const cake = new TaskItem('make the cake');
-const decore = new TaskItem('decorate house');
-birthdayTasks.taskItems.push(cake);
-birthdayTasks.taskItems.push(decore);
-
-lawnAndGarden.taskItems.forEach(taskItem => console.log(taskItem.taskName));
-houseChores.taskItems.forEach(taskItem => console.log(taskItem.taskName));
-birthdayTasks.taskItems.forEach(taskItem => console.log(taskItem.taskName));
-
-
-
-taskGroupsDB.push(birthdayTasks);
-taskGroupsDB.push(lawnAndGarden);
-console.log(taskGroupsDB)  */
+const todoApp = new TodoApp();
+todoApp.start();
