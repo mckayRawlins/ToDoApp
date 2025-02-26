9e8718b9fe0aa55f7f8d0e2b7214841e32135f82
Miriam Rawlins <miriammrawlins@gmail.com>
2025-02-25 19:25:21 -0700

    adding edits task buttons

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,23 +5,26 @@ class TodoApp {
         const taskGroupUl = this.taskGroupUl();
         taskGroupUl.innerHTML = '';
         this.taskItemsUl().innerHTML = '';
-    
+
         this.taskGroupsDB.forEach(taskGroup => {
             const taskGroupLi = document.createElement('li');
             taskGroupLi.taskGroup = taskGroup;
             taskGroupLi.innerText = taskGroup.name;
-            
+
             if (taskGroup.selected) {
                 taskGroupLi.classList.add('selected');
-                var taskGroupNameElement = this.getElement('task-group-name');
-                taskGroupNameElement.innerText = taskGroup.name;
-                taskGroup.taskItems.forEach(this.renderTaskItems.bind(this));
+                this.taskGroupNameElement().innerText = taskGroup.name;
+                taskGroup.taskItems.forEach(this.renderTaskItems.bind(this)); //google .bind
             }
- 
+
             taskGroupUl.appendChild(taskGroupLi);
             taskGroupLi.addEventListener('click', this.taskGroupLiClicked.bind(this));
         });
-        
+
+        if (this.taskGroupsDB.length === 0) {
+            this.taskGroupNameElement().innerText = 'No task group selected';
+        }
+
         this.save();
     }
 
@@ -33,18 +36,21 @@ class TodoApp {
         checkbox.type = 'checkbox';
 
         if (taskItem.completed) {
-            checkbox.checked = true; 
+            checkbox.checked = true;
         }
 
-        checkbox.addEventListener('click', () => {
-            taskItem.completed = !taskItem.completed;
-        });
+        checkbox.addEventListener('click', () => taskItem.completed = !taskItem.completed);
 
         const nameSpan = document.createElement('span');
         nameSpan.innerHTML = taskItem.name;
 
+        const editTaskButton = document.createElement('span');
+        editTaskButton.classList.add('fa');
+        editTaskButton.classList.add('fa-pencil-alt');
+
         taskItemLi.appendChild(checkbox);
         taskItemLi.appendChild(nameSpan);
+        taskItemLi.appendChild(editTaskButton);
 
         this.taskItemsUl().appendChild(taskItemLi);
     }
@@ -58,18 +64,21 @@ class TodoApp {
 
     removeTaskGroup() {
         const selectedTaskGroup = this.selectedTaskGroup();
-        this.taskGroupsDB = this.taskGroupsDB.filter(taskGroup => taskGroup.name !== selectedTaskGroup.name);
+        const remainingTaskGroups = this.taskGroupsDB.filter(taskGroup => taskGroup.name !== selectedTaskGroup.name);
 
-        if (this.taskGroupsDB.length > 0) {
-            this.taskGroupsDB.forEach(taskGroup => taskGroup.selected = false);
-            this.taskGroupsDB[0].selected = true;
+        if (remainingTaskGroups.length > 0) {
+            remainingTaskGroups.forEach(taskGroup => taskGroup.selected = false);
+            remainingTaskGroups[0].selected = true;
             this.save();
         } else {
             this.load();
         }
 
+        this.taskGroupsDB = remainingTaskGroups;
+
         this.renderUI();
     }
+
 
     taskGroupLiClicked(mouseEvent) {
         for (let taskGroupLi of this.taskGroupUl().children) {
@@ -78,16 +87,17 @@ class TodoApp {
         }
 
         const taskGroupLi = mouseEvent.target;
-        taskGroupLi.taskGroup.selected = true;       
+        taskGroupLi.taskGroup.selected = true;
         taskGroupLi.classList.add('selected');
-            
+
         this.renderUI()
     }
 
     addGroup() {
         const newTaskGroupInput = this.getElement('new-group-input');
+        const existingTaskGroup = this.taskGroupsDB.find(taskGroup => taskGroup.name === newTaskGroupInput.value);
 
-        if (newTaskGroupInput.value.length > 0) {
+        if (newTaskGroupInput.value.length > 0 && !existingTaskGroup) {
             this.taskGroupsDB.forEach(taskGroup => taskGroup.selected = false)
 
             const newTaskGroup = new TaskGroup(newTaskGroupInput.value);
@@ -110,9 +120,14 @@ class TodoApp {
 
     getElement(id) {
         return document.getElementById(id);
-    } 
+    }
+
+    taskGroupNameElement() {
+        return this.getElement('task-group-name');
+    }
 
     taskGroupUl() {
+
         return this.getElement('task-group-ul');
     }
 
@@ -132,7 +147,7 @@ class TodoApp {
         const savedTaskGroups = localStorage.getItem('task-groups');
 
         if (savedTaskGroups === null) {
-            localStorage.setItem('task-groups', JSON.stringify([defaultTaskGroup()]));
+            localStorage.setItem('task-groups', JSON.stringify([this.defaultTaskGroup()]));
         }
 
         this.taskGroupsDB = JSON.parse(localStorage.getItem('task-groups'));
